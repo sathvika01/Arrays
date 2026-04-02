7ae84f43c95f26ec43d43d3dabc72da97ca1426f
sathvika01 <sathvikabilla01@gmail.com>
2026-04-02 16:02:04 -0500

    Move Zeroes

--- FastSlowPointer.py
+++ FastSlowPointer.py
@@ -14,4 +14,15 @@ def removeDuplicates(self, nums: List[int]) -> int:
                 nums[slow] = nums[fast]
 
         return slow + 1
+
+# Move Zeroes
+def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        slow = 0
+        for fast in range(1,len(nums)):
+            if nums[slow] == 0 and nums[fast] !=0:
+                nums[slow],nums[fast] = nums[fast],nums[slow]
+                slow += 1
         
